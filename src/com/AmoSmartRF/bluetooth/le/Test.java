243ef9b4489c5a0c37ab58e025aa3f319fb6d20d
james <chinesecaijames@gmail.com>
2017-09-30 18:01:59 +0800

    change ui

---
+++ src/com/AmoSmartRF/bluetooth/le/Test.java
@@ -0,0 +1,16 @@
+package com.AmoSmartRF.bluetooth.le;
+
+import com.lin.bluetooth.le.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Test extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_custom);
+    }
+
+}
